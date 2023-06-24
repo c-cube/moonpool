f9b85bc0af65b6357a20ad7702462bd719e067c4
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-06-23 23:18:52 -0400

    add `Fork_join.all_{list,init}`
    primitives to fork-join over n tasks

--- src/fork_join.ml
+++ src/fork_join.ml
@@ -86,3 +86,45 @@ let both f g : _ * _ =
           start_tasks ~run ());
     };
   get_exn st
+
+let all_list fs : _ list =
+  let len = List.length fs in
+  let arr = Array.make len None in
+  let has_failed = A.make false in
+  let missing = A.make len in
+
+  let start_tasks ~run (suspension : Suspend_types_.suspension) =
+    let task_for i f =
+      try
+        let x = f () in
+        arr.(i) <- Some x;
+
+        if A.fetch_and_add missing (-1) = 1 then
+          (* all tasks done successfully *)
+          suspension (Ok ())
+      with exn ->
+        let bt = Printexc.get_raw_backtrace () in
+        if not (A.exchange has_failed true) then
+          (* first one to fail, and [missing] must be >= 2
+             because we're not decreasing it. *)
+          suspension (Error (exn, bt))
+    in
+
+    List.iteri (fun i f -> run ~with_handler:true (fun () -> task_for i f)) fs
+  in
+
+  Suspend_.suspend
+    {
+      Suspend_types_.handle =
+        (fun ~run suspension ->
+          (* nothing else is started, no race condition possible *)
+          start_tasks ~run suspension);
+    };
+
+  (* get all results *)
+  List.init len (fun i ->
+      match arr.(i) with
+      | None -> assert false
+      | Some x -> x)
+
+let all_init n f = all_list @@ List.init n (fun i () -> f i)
