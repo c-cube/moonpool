cfbcc726489d698a6db7f1f10e04914dbfbee826
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-08-13 22:47:54 -0400

    improve a bit the resource test

--- test/t_resource.ml
+++ test/t_resource.ml
@@ -6,6 +6,9 @@ let ( let@ ) = ( @@ )
 let () =
   let a = Atomic.make 0 in
   for _i = 1 to 1_000 do
+    (* give a chance to domains to die *)
+    if _i mod 100 = 0 then Thread.delay 0.8;
+
     (* allocate a new pool at each iteration *)
     let@ p = Pool.with_ ~min:4 () in
     Pool.run_wait_block p (fun () -> Atomic.incr a)
