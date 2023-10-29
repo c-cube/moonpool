5a2b8290b4c2b64ce8cb5af4eddeee89bf349f01
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-10-29 16:41:19 -0400

    refactor: use a fixed size work-stealing deque
    if it's full, tasks overflow into the main queue.

--- src/ws_deque_.mli
+++ src/ws_deque_.mli
@@ -6,14 +6,16 @@
 type 'a t
 (** Deque containing values of type ['a] *)
 
-val create : unit -> _ t
+val create : dummy:'a -> unit -> 'a t
 (** Create a new deque. *)
 
-val push : 'a t -> 'a -> unit
-(** Push value at the bottom of deque. This is not thread-safe. *)
+val push : 'a t -> 'a -> bool
+(** Push value at the bottom of deque. returns [true] if it succeeds.
+    This must be called only by the owner thread. *)
 
 val pop : 'a t -> 'a option
-(** Pop value from the bottom of deque. This is not thread-safe. *)
+(** Pop value from the bottom of deque.
+    This must be called only by the owner thread. *)
 
 val steal : 'a t -> 'a option
 (** Try to steal from the top of deque. This is thread-safe. *)
