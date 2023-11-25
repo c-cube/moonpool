16663651d6c515ab3cb3feaaf80d4b6da1647e8f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-11-24 23:12:20 -0500

    update test

--- test/t_ws_wait.ml
+++ test/t_ws_wait.ml
@@ -4,12 +4,12 @@ let ( let@ ) = ( @@ )
 
 let () =
   let sum = Atomic.make 0 in
-  let n = 300_000 in
+  let n = 100_000 in
 
   (let@ pool = Ws_pool.with_ ~num_threads:30 () in
    for _i = 1 to n do
      Runner.run_async pool (fun () ->
-         Thread.yield ();
+         Thread.delay 0.0001;
          ignore (Atomic.fetch_and_add sum 20 : int))
    done;
    Runner.shutdown pool);
@@ -17,5 +17,8 @@ let () =
   (* make sure that shutdown didn't terminate before
      all tasks were run *)
   let sum = Atomic.get sum in
-  assert (sum = 20 * n);
+  let expected = 20 * n in
+
+  Printf.printf "sum=%d, expected=%d\n%!" sum expected;
+  assert (sum = expected);
   ()
