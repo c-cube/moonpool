bed47fc79522a80fa459942b4fec399cf70febd0
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-02-03 17:21:40 -0500

    expose Tracing

--- src/private/tracing_.dummy.ml
+++ src/private/tracing_.dummy.ml
@@ -3,3 +3,4 @@ let dummy_span = 0L
 let enter_span _name = dummy_span
 let exit_span = ignore
 let set_thread_name = ignore
+let with_span _ f = f dummy_span
--- src/private/tracing_.real.ml
+++ src/private/tracing_.real.ml
@@ -12,3 +12,14 @@ let[@inline] enter_span name : int64 =
     Trace.enter_span ~__FILE__:dummy_file_ ~__LINE__:0 name
 
 let[@inline] exit_span sp = if sp <> dummy_span then Trace.exit_span sp
+
+let with_span name f =
+  let sp = enter_span name in
+  try
+    let x = f sp in
+    exit_span sp;
+    x
+  with exn ->
+    let bt = Printexc.get_raw_backtrace () in
+    exit_span sp;
+    Printexc.raise_with_backtrace exn bt
