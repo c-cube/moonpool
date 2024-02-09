d248a569f6deaf7554be1f48837cdd913de004f4
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-02-08 21:02:19 -0500

    feat: progress on moonpool-lwt

--- src/lwt/common_.ml
+++ src/lwt/common_.ml
@@ -2,7 +2,3 @@ module M = Moonpool
 module Exn_bt = M.Exn_bt
 
 let ( let@ ) = ( @@ )
-
-let[@inline] cancel_handle_of_event (ev : Lwt_engine.event) : Cancel_handle.t =
-  let cancel () = Lwt_engine.stop_event ev in
-  { Cancel_handle.cancel }
--- src/lwt/moonpool_lwt.mli
+++ src/lwt/moonpool_lwt.mli
@@ -1,14 +1,24 @@
 (** Lwt_engine-based event loop for Moonpool *)
 
-module Cancel_handle = Cancel_handle
 module Fiber = Moonpool_fib.Fiber
 module FLS = Moonpool_fib.Fls
 
+(** {2 Basic conversions} *)
+
 val fut_of_lwt : 'a Lwt.t -> 'a Moonpool.Fut.t
+(** [fut_of_lwt lwt_fut] makes a thread-safe moonpool future that
+    completes when [lwt_fut] does *)
+
+val lwt_of_fut : 'a Moonpool.Fut.t -> 'a Lwt.t
+(** [lwt_of_fut fut] makes a lwt future that completes when
+    [fut] does. The result should be used only from inside the
+    thread running [Lwt_main.run]. *)
+
+(** {2 Helpers on the moonpool side} *)
 
 val await_lwt : 'a Lwt.t -> 'a
 (** [await_lwt fut] awaits a Lwt future from inside a task running on
-    a moonpool runner. *)
+    a moonpool runner. This must be run from within moonpool. *)
 
 val run_in_lwt : (unit -> 'a Lwt.t) -> 'a Moonpool.Fut.t
 (** [run_in_lwt f] runs [f()] from within the Lwt thread
@@ -22,6 +32,22 @@ val get_runner : unit -> Moonpool.Runner.t
 (** Returns the runner from within which this is called.
     Must be run from within a fiber.
     @raise Failure if not run within a fiber *)
+
+module IO : sig
+  val read : Unix.file_descr -> bytes -> int -> int -> int
+  val write_once : Unix.file_descr -> bytes -> int -> int -> int
+  val write : Unix.file_descr -> bytes -> int -> int -> unit
+  val sleep_s : float -> unit
+end
+
+(** {2 Helpers on the lwt side} *)
+
+val detach_in_runner : runner:Moonpool.Runner.t -> (unit -> 'a) -> 'a Lwt.t
+(** [detach_in_runner ~runner f] runs [f] in the given moonpool runner,
+    and returns a lwt future. This must be run from within the thread
+    running [Lwt_main]. *)
+
+(** {2 Wrappers around Lwt_main} *)
 
 val main_with_runner : runner:Moonpool.Runner.t -> (unit -> 'a) -> 'a
 (** [main_with_runner ~runner f] starts a Lwt-based event loop and runs [f()] inside
