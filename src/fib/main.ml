48fbf876dcf757b078d34bc8ad8a9ec7dcc7072e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-03-04 15:48:45 -0500

    feat: add `Moonpool_fib.main`
    this is a convenient entrypoint for programs that use fibers. The main
    thread can thus await fibers that run in the background.

---
+++ src/fib/main.ml
@@ -0,0 +1,16 @@
+exception Oh_no of Exn_bt.t
+
+let main (f : Runner.t -> 'a) : 'a =
+  let st = Fifo_pool.Private_.create_state ~threads:[| Thread.self () |] () in
+  let runner = Fifo_pool.Private_.runner_of_state st in
+  try
+    let fiber = Fiber.spawn_top ~on:runner (fun () -> f runner) in
+    Fiber.on_result fiber (fun _ -> Runner.shutdown_without_waiting runner);
+    (* run the main thread *)
+    Fifo_pool.Private_.run_thread st runner ~on_exn:(fun e bt ->
+        raise (Oh_no (Exn_bt.make e bt)));
+    match Fiber.peek fiber with
+    | Some (Ok x) -> x
+    | Some (Error ebt) -> Exn_bt.raise ebt
+    | None -> assert false
+  with Oh_no ebt -> Exn_bt.raise ebt
