fd89c81f570d82562a23607e3084407d6f1187fe
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-09-04 10:18:53 -0400

    add Event.Infix

--- src/sync/event.mli
+++ src/sync/event.mli
@@ -3,3 +3,10 @@ include module type of struct
 end
 
 val of_fut : 'a Moonpool.Fut.t -> 'a t
+
+module Infix : sig
+  val ( >|= ) : 'a t -> ('a -> 'b) -> 'b t
+  val ( let+ ) : 'a t -> ('a -> 'b) -> 'b t
+end
+
+include module type of Infix
