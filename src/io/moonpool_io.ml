9623e2d4b66edcc029aea95a1e413005ca36895e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-09-05 13:24:17 -0400

    add `Moonpool_io.main`; make `configure` idempotent

--- src/io/moonpool_io.ml
+++ src/io/moonpool_io.ml
@@ -3,7 +3,20 @@ module Unix = Picos_io.Unix
 module Select = Picos_io_select
 
 let fd_of_unix_fd : Unix.file_descr -> Fd.t = Fun.id
-let configure = Select.configure
+
+open struct
+  let has_configured = Atomic.make false
+end
+
+let configure ?intr_sig ?handle_sigchld ?ignore_sigpipe () : unit =
+  if not (Atomic.exchange has_configured true) then
+    Select.configure ?intr_sig ?handle_sigchld ?ignore_sigpipe ()
+
+(** [main f] runs [f()] inside a scheduler. *)
+let main ?intr_sig ?handle_sigchld ?ignore_sigpipe (f : Moonpool.Runner.t -> 'a)
+    : 'a =
+  configure ?intr_sig ?handle_sigchld ?ignore_sigpipe ();
+  Moonpool_fib.main f
 
 (** {2 Async read/write} *)
 
