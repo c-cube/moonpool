d7c8df43d9529764f98b4d95f273931dc320669e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-09-05 13:49:17 -0400

    no need to `configure` for us

--- src/io/moonpool_io.ml
+++ src/io/moonpool_io.ml
@@ -4,19 +4,8 @@ module Select = Picos_io_select
 
 let fd_of_unix_fd : Unix.file_descr -> Fd.t = Fun.id
 
-open struct
-  let has_configured = Atomic.make false
-end
-
-let configure ?intr_sig ?handle_sigchld ?ignore_sigpipe () : unit =
-  if not (Atomic.exchange has_configured true) then
-    Select.configure ?intr_sig ?handle_sigchld ?ignore_sigpipe ()
-
 (** [main f] runs [f()] inside a scheduler. *)
-let main ?intr_sig ?handle_sigchld ?ignore_sigpipe (f : Moonpool.Runner.t -> 'a)
-    : 'a =
-  configure ?intr_sig ?handle_sigchld ?ignore_sigpipe ();
-  Moonpool_fib.main f
+let main (f : Moonpool.Runner.t -> 'a) : 'a = Moonpool_fib.main f
 
 (** {2 Async read/write} *)
 
