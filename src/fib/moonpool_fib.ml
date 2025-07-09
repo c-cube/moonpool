83acc18d3d21f7bf4f24f4f9c3ceec21b7080306
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-07-09 15:28:33 -0400

    deprecate fibers

--- src/fib/moonpool_fib.ml
+++ src/fib/moonpool_fib.ml
@@ -5,8 +5,15 @@
     @since 0.6. *)
 
 module Fiber = Fiber
+[@@deprecated "use picos structured concurrency or something else"]
+
 module Fls = Fls
 module Handle = Handle
+
 module Main = Main
+[@@deprecated "use picos structured concurrency or something else"]
+
+[@@@ocaml.alert "-deprecated"]
+
 include Fiber
 include Main
