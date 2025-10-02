538f3df31abbc07974666ac5beeda1f25a6de51b
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-10-02 14:37:15 -0400

    doc correction

--- src/lwt/moonpool_lwt.mli
+++ src/lwt/moonpool_lwt.mli
@@ -17,8 +17,7 @@ module Fut = Moonpool.Fut
 
 val fut_of_lwt : 'a Lwt.t -> 'a Moonpool.Fut.t
 (** [fut_of_lwt lwt_fut] makes a thread-safe moonpool future that completes when
-    [lwt_fut] does. This must be run from within the Lwt thread, eg inside of
-    {!spawn_lwt} or {!run_in_lwt_and_await}. *)
+    [lwt_fut] does. This can be run from any thread. *)
 
 val lwt_of_fut : 'a Moonpool.Fut.t -> 'a Lwt.t
 (** [lwt_of_fut fut] makes a lwt future that completes when [fut] does. This
