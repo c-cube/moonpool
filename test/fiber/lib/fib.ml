f8e553d4739e6325fcef339e62b4dd4f755359d2
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-10-22 11:59:35 -0400

    test: update tests, removing the fibers and cancellation tests

--- test/fiber/lib/fib.ml
+++ test/fiber/lib/fib.ml
@@ -1,6 +1,7 @@
-open! Moonpool
+module Chan = Moonpool.Chan
+module Exn_bt = Moonpool.Exn_bt
 module A = Atomic
-module F = Moonpool_fib.Fiber
+module Fut = Moonpool.Fut
 
 let ( let@ ) = ( @@ )
 
@@ -49,24 +50,23 @@ let logf = Log_.logf
 let run1 ~runner () =
   Printf.printf "============\nstart\n%!";
   let clock = ref TS.init in
-  let fib =
-    F.spawn_top ~on:runner @@ fun () ->
+  let fut =
+    Fut.spawn ~on:runner @@ fun () ->
     let chan_progress = Chan.create ~max_size:4 () in
     let chans = Array.init 5 (fun _ -> Chan.create ~max_size:4 ()) in
 
     let subs =
       List.init 5 (fun i ->
-          F.spawn ~protect:false @@ fun _n ->
+          Fut.spawn ~on:runner @@ fun _n ->
           Thread.delay (float i *. 0.01);
           Chan.pop chans.(i);
           Chan.push chan_progress i;
-          F.check_if_cancelled ();
           i)
     in
 
     logf (TS.tick_get clock) "wait for subs";
 
-    F.spawn_ignore (fun () ->
+    Moonpool.run_async runner (fun () ->
         for i = 0 to 4 do
           Chan.push chans.(i) ();
           let i' = Chan.pop chan_progress in
@@ -78,19 +78,15 @@ let run1 ~runner () =
        (fun i f ->
          let clock = ref (0 :: i :: clock0) in
          logf !clock "await fiber %d" i;
-         logf (TS.tick_get clock) "cur fiber[%d] is some: %b" i
-           (Option.is_some @@ F.Private_.get_cur_opt ());
-         let res = F.await f in
-         logf (TS.tick_get clock) "cur fiber[%d] is some: %b" i
-           (Option.is_some @@ F.Private_.get_cur_opt ());
-         F.yield ();
+         let res = Fut.await f in
+         Fut.yield ();
          logf (TS.tick_get clock) "res %d = %d" i res)
        subs);
 
     logf (TS.tick_get clock) "main fiber done"
   in
 
-  Fut.await @@ F.res fib;
+  Fut.await fut;
   logf (TS.tick_get clock) "main fiber exited";
   Log_.print_and_clear ();
   ()
@@ -99,15 +95,11 @@ let run2 ~runner () =
   (* same but now, cancel one of the sub-fibers *)
   Printf.printf "============\nstart\n";
 
-  let clock = ref TS.init in
-  let fib =
-    F.spawn_top ~on:runner @@ fun () ->
-    let@ () =
-      F.with_on_self_cancel (fun ebt ->
-          logf (TS.tick_get clock) "main fiber cancelled with %s"
-          @@ Exn_bt.show ebt)
-    in
+  let to_await = ref [] in
 
+  let clock = ref TS.init in
+  let fut =
+    Fut.spawn ~on:runner @@ fun () ->
     let chans_unblock = Array.init 10 (fun _i -> Chan.create ~max_size:4 ()) in
     let chan_progress = Chan.create ~max_size:4 () in
 
@@ -116,11 +108,7 @@ let run2 ~runner () =
       let clock0 = !clock in
       List.init 10 (fun i ->
           let clock = ref (0 :: i :: clock0) in
-          F.spawn ~protect:false @@ fun _n ->
-          let@ () =
-            F.with_on_self_cancel (fun _ ->
-                logf (TS.tick_get clock) "sub-fiber %d was cancelled" i)
-          in
+          Fut.spawn ~on:runner @@ fun _n ->
           Thread.delay 0.002;
 
           (* sync for determinism *)
@@ -132,46 +120,51 @@ let run2 ~runner () =
             failwith "oh no!"
           );
 
-          F.check_if_cancelled ();
           i)
     in
 
     let post = TS.tick_get clock in
     List.iteri
       (fun i fib ->
-        F.on_result fib (function
+        Fut.on_result fib (function
           | Ok _ -> logf (i :: post) "fiber %d resolved as ok" i
           | Error _ -> logf (i :: post) "fiber %d resolved as error" i))
       subs;
 
     (* sequentialize the fibers, for determinism *)
-    F.spawn_ignore (fun () ->
-        for j = 0 to 9 do
-          Chan.push chans_unblock.(j) ();
-          let j' = Chan.pop chan_progress in
-          assert (j = j')
-        done);
+    let sender =
+      Fut.spawn ~on:runner (fun () ->
+          for j = 0 to 9 do
+            Chan.push chans_unblock.(j) ();
+            let j' = Chan.pop chan_progress in
+            assert (j = j')
+          done)
+    in
+    to_await := sender :: !to_await;
 
     logf (TS.tick_get clock) "wait for subs";
     List.iteri
       (fun i f ->
         logf (TS.tick_get clock) "await fiber %d" i;
-        let res = F.await f in
+        let res = Fut.await f in
         logf (TS.tick_get clock) "res %d = %d" i res)
       subs;
     logf (TS.tick_get clock) "yield";
-    F.yield ();
+    Fut.yield ();
     logf (TS.tick_get clock) "yielded";
     logf (TS.tick_get clock) "main fiber done"
   in
 
-  F.on_result fib (function
+  Fut.on_result fut (function
     | Ok () -> logf (TS.tick_get clock) "main fiber result: ok"
     | Error ebt ->
       logf (TS.tick_get clock) "main fiber result: error %s" (Exn_bt.show ebt));
 
-  (try Fut.await @@ F.res fib
+  (try Fut.await fut
    with Failure msg -> logf (TS.tick_get clock) "main fib failed with %S" msg);
   logf (TS.tick_get clock) "main fiber exited";
+
+  List.iter Fut.await !to_await;
+
   Log_.print_and_clear ();
   ()
